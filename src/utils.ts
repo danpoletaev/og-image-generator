c69b8a8e0f43c08fcfb46f6b96d8af58ae817f37
Daniil Poletaev <44584010+danpoletaev@users.noreply.github.com>
2025-01-03 17:11:45 +0100

    fix: replace all occurences

--- src/utils.ts
+++ src/utils.ts
@@ -20,7 +20,7 @@ export const prepareHtml = ({ html, templateData }: ActorInput) => {
     let finalHtml = html;
 
     Object.entries(templateData).forEach(([key, value]) => {
-        finalHtml = finalHtml.replace(`{{${key}}}g`, value);
+        finalHtml = finalHtml.replace(new RegExp(`{{${key}}}`, 'g'), value);
     });
 
     return finalHtml;
